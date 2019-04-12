0f7a6c6dafd4017245cf3f4f83f9cb2578d0f0e6
Justin Johns <johnsju@mail.gvsu.edu>
2019-04-12 12:47:10 -0400

    Initial project upload

---
+++ src/router.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Signup from './views/Signup.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/home',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/',
+      name: 'Login',
+      component: Login  
+    },
+    {
+      path: '/sign-up',
+      name: 'Signup',
+      component: Signup
+    }
+  ]
+})
---
+++ src/views/Signup.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="sign-up">
+        <p>Join the club!</p>
+        <input type="text" placeholder="Username"><br>
+        <input type="password" placeholder="Password"><br>
+        <button @click="signUp">Sign Up</button>
+        <span>or go </span><router-link to="/login">back.</router-link>
+    </div>
+</template>
+
+<script>
+import firebase from 'firebase';
+//import func from '../vue-temp/vue-editor-bridge';
+
+export default {
+    name: 'signUp',
+    data(){
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        signUp: function() {
+            firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
+                function (user){
+                    alert('Your account has been created!')
+                },
+                function (err){
+                    alert('Yikes.. ' + err.message)
+                }
+            )
+        }
+    }
+}
+</script>
+<style scoped>
+  .signUp {
+        margin-top: 40px;
+    }
+    input {
+        margin: 10px 0;
+        width: 20%;
+        padding: 15px;
+    }
+    button {
+        margin-top: 10px;
+        width: 10%;
+        cursor: pointer;
+    }
+    span {
+        display: block;
+        margin-top: 20px;
+        font-size: 11px;
+    }
+</style>
+
